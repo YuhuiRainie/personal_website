01c9f504aa8666f08c096063fda0ad56fc65ca07
YuhuiRainie <48782788+YuhuiRainie@users.noreply.github.com>
2020-09-11 17:57:36 +1000

    upload header

---
+++ src/components/Header.js
@@ -0,0 +1,34 @@
+import React from 'react'
+import { Button } from './Button'
+import './Header.css'
+import '../App.css'
+
+function Header () {
+  return (
+    <div className='header-container'>
+      <video src='/motionplaces fire-Oct2018.mp4' autoPlay loop muted />
+      <h1>Rainie Liu</h1>
+      <p>Software Enginner based on Sydney</p>
+      <p>The days break you are the days make you</p>
+      <div className='header-btns'>
+        <Button
+          className='btns'
+          buttonStyle='btn--outline'
+          buttonSize='btn--large'
+        >
+        See My Projects
+        </Button>
+        <Button
+          className='btns'
+          buttonStyle='btn--primary'
+          buttonSize='btn--large'
+          onClick={console.log('hey')}
+        >
+         See My CV <i className='fas fa-file-alt' />
+        </Button>
+      </div>
+    </div>
+  )
+}
+
+export default Header
---
+++ src/components/pages/Home.js
@@ -0,0 +1,14 @@
+import '../../App.css'
+import Header from '../Header'
+import React from 'react'
+// import Card from '../Card'
+
+function Home () {
+  return (
+    <div>
+      <Header />
+      {/* <Card /> */}
+    </div>
+  )
+}
+export default Home
